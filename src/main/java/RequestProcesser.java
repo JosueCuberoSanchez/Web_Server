3f598ae1d7088cd258f40a9d1d7a4977b4e4cb29
josue.cubero@ucr.ac.cr <josue.cubero@ucr.ac.cr>
2018-03-21 18:41:59 -0600

    Ordenado

--- src/main/java/RequestProcesser.java
+++ src/main/java/RequestProcesser.java
@@ -9,12 +9,24 @@ import java.io.OutputStream;
  * Created by josue on 21/03/18.
  */
 public class RequestProcesser implements HttpHandler {
+
     public void handle(HttpExchange t) throws IOException {
+        /*String method = t.getRequestMethod();
+        if(method.equalsIgnoreCase("POST")) {
+
+        } else if(method.equalsIgnoreCase("GET")){
+
+        } else if(method.equalsIgnoreCase("HEAD")){
+
+        } else { //501
+
+        }*/
         String response = "12345";
         Headers headers = t.getResponseHeaders();
+        String path = t.getHttpContext().getPath();
         headers.add("Server", "MonkeyLabsServer");
         headers.add("Content-Type", "text/html");
-        t.sendResponseHeaders(200, response.length());
+        t.sendResponseHeaders(501, response.length());
         OutputStream os = t.getResponseBody();
         os.write(response.getBytes());
         os.close();
